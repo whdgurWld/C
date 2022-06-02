e27b4d2ab9d8d44ebd097201fdeb1be82289076d
whdgurWld <73575138+whdgurWld@users.noreply.github.com>
2022-06-02 22:06:41 +0900

    Update 1110.c

--- 1110.c
+++ 1110.c
@@ -5,7 +5,7 @@ int main(void) {
   int tenth = 1;
   int first = 1;
   int add = 1;
-  int new_num = 1;
+  int new_num;
   int input_save;
   int tries = 0;
   
@@ -13,13 +13,16 @@ int main(void) {
 
   input_save = input;
 
-  while(new_num != input_save){
+  while(1){
     tenth = input/10;
     first = input % 10;
     add = (tenth+first) % 10;
     new_num = (first*10) + add;
     input = new_num;
     tries++;
+    if(new_num==input_save){
+      break;
+    }
   }
   printf("%d", tries);
  return(0);
